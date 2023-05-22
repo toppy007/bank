57c3aa22fdb97e05b6ee8fc3fe18f1b3363b4350
toppy007 <33899845+toppy007@users.noreply.github.com>
2023-05-22 08:16:26 +0100

    project setup

---
+++ transaction.js
@@ -0,0 +1,17 @@
+class Transactions {
+    constructor () {
+        this.balance = 0
+    };
+
+    deposit(add) {
+        return this.balance + add
+    }
+
+    withdraw(minus) {
+        return this.balance - minus
+    }
+}
+
+module.exports = Transactions
+
+
